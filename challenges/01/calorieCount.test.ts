f05bd3b8ed605d94e28fb84f693a8079a85bdd68
defaude <defaude@gmail.com>
2022-12-01 20:31:39 +0100

    [CHALLENGE 01-2] Add implementation

--- challenges/01/calorieCount.test.ts
+++ challenges/01/calorieCount.test.ts
@@ -1,16 +1,30 @@
 import { describe, expect, it } from 'vitest';
-import { maxCalories } from './calorieCount';
+import { maxCalories, topThreeCalories } from './calorieCount';
 import { getFileLines } from '../getFileLines';
 
-describe('calorieCount', () => {
-    it('should solve the example data', async () => {
-        const lines = await getFileLines('exampleInput.txt', import.meta.url);
-        expect(maxCalories(lines)).toBe(24000);
+describe('01', async () => {
+    const example = await getFileLines('exampleInput.txt', import.meta.url);
+    const data = await getFileLines('input.txt', import.meta.url);
+
+    describe('maxCalories', () => {
+        it('should create the expected result for example data', async () => {
+            expect(maxCalories(example)).toBe(24000);
+        });
+
+        it('should produce the solution', async () => {
+            const result = maxCalories(data);
+            console.info('01-1 maxCalories', result);
+        });
     });
 
-    it('should produce the solution', async () => {
-        const lines = await getFileLines('input.txt', import.meta.url);
-        const result = maxCalories(lines);
-        console.info('01 calorieCount', result);
+    describe('topThreeCalories', () => {
+        it('should create the expected result for example data', () => {
+            expect(topThreeCalories(example)).toBe(45000);
+        });
+
+        it('should produce the solution', async () => {
+            const result = topThreeCalories(data);
+            console.info('01-2 topThreeCalories', result);
+        });
     });
 });
--- challenges/01/calorieCount.ts
+++ challenges/01/calorieCount.ts
@@ -1,15 +1,34 @@
-export const maxCalories = (lines: string[]): number => {
+export const getItems = (lines: string[]): number[] => {
+    return lines.map((x) => parseInt(x, 10));
+};
+
+export const getElfTotals = (items: number[]): number[] => {
     const elves = [0];
 
-    const asNumbers = lines.map((x) => parseInt(x, 10));
-    for (const n of asNumbers) {
-        if (isNaN(n)) {
-            // start a new elf
+    for (const item of items) {
+        if (isNaN(item)) {
             elves.push(0);
         } else {
-            elves[elves.length - 1] += n;
+            elves[elves.length - 1] += item;
         }
     }
 
+    return elves;
+};
+
+export const maxCalories = (lines: string[]): number => {
+    const items = getItems(lines);
+    const elves = getElfTotals(items);
+
     return Math.max(...elves);
+};
+
+export const topThreeCalories = (lines: string[]): number => {
+    const items = getItems(lines);
+    const elves = getElfTotals(items);
+
+    elves.sort((a, b) => b - a);
+    const [a, b, c] = elves;
+
+    return a + b + c;
 };
