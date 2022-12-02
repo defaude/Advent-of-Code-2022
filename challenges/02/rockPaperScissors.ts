f94ae0c99c6d50d85005b54e9b9d588405ec04f1
defaude <defaude@gmail.com>
2022-12-02 17:08:08 +0100

    [CHALLENGE 02-2] Refactor implementation

--- challenges/02/rockPaperScissors.ts
+++ challenges/02/rockPaperScissors.ts
@@ -1,16 +1,45 @@
-import { assertNever } from '../../utility/assertNever';
-
-enum Opponent {
+enum OpponentMove {
     ROCK = 'A',
     PAPER = 'B',
     SCISSORS = 'C',
 }
 
-enum Player {
+enum PlayerMove {
     ROCK = 'X',
     PAPER = 'Y',
     SCISSORS = 'Z',
 }
+
+const roundScores = {
+    [OpponentMove.ROCK]: {
+        [PlayerMove.ROCK]: 3,
+        [PlayerMove.PAPER]: 6,
+        [PlayerMove.SCISSORS]: 0,
+    },
+    [OpponentMove.PAPER]: {
+        [PlayerMove.ROCK]: 0,
+        [PlayerMove.PAPER]: 3,
+        [PlayerMove.SCISSORS]: 6,
+    },
+    [OpponentMove.SCISSORS]: {
+        [PlayerMove.ROCK]: 6,
+        [PlayerMove.PAPER]: 0,
+        [PlayerMove.SCISSORS]: 3,
+    },
+};
+
+const getRoundScore = ([opponent, player]: Round) =>
+    roundScores[opponent][player];
+
+const moveScores = {
+    [PlayerMove.ROCK]: 1,
+    [PlayerMove.PAPER]: 2,
+    [PlayerMove.SCISSORS]: 3,
+};
+
+const getMoveScore = (player: PlayerMove) => moveScores[player];
+
+type Round = [OpponentMove, PlayerMove];
 
 enum DesiredOutcome {
     LOSE = 'X',
@@ -18,8 +47,28 @@ enum DesiredOutcome {
     WIN = 'Z',
 }
 
-type Round = [Opponent, Player];
-type DynamicRound = [Opponent, DesiredOutcome];
+const movesForOutcome = {
+    [OpponentMove.ROCK]: {
+        [DesiredOutcome.LOSE]: PlayerMove.SCISSORS,
+        [DesiredOutcome.DRAW]: PlayerMove.ROCK,
+        [DesiredOutcome.WIN]: PlayerMove.PAPER,
+    },
+    [OpponentMove.PAPER]: {
+        [DesiredOutcome.LOSE]: PlayerMove.ROCK,
+        [DesiredOutcome.DRAW]: PlayerMove.PAPER,
+        [DesiredOutcome.WIN]: PlayerMove.SCISSORS,
+    },
+    [OpponentMove.SCISSORS]: {
+        [DesiredOutcome.LOSE]: PlayerMove.PAPER,
+        [DesiredOutcome.DRAW]: PlayerMove.SCISSORS,
+        [DesiredOutcome.WIN]: PlayerMove.ROCK,
+    },
+};
+
+type DynamicRound = [OpponentMove, DesiredOutcome];
+
+const getMoveForOutcome = ([opponent, desiredOutcome]: DynamicRound) =>
+    movesForOutcome[opponent][desiredOutcome];
 
 const lineRgx = /^[ABC] [XYZ]$/;
 
@@ -29,69 +78,20 @@ const getRounds = <T extends Round | DynamicRound>(lines: string[]) => {
         .map((line) => line.split(' ')) as unknown as T[];
 };
 
-const moves = {
-    [Opponent.ROCK]: {
-        [Player.ROCK]: 3,
-        [Player.PAPER]: 6,
-        [Player.SCISSORS]: 0,
-    },
-    [Opponent.PAPER]: {
-        [Player.ROCK]: 0,
-        [Player.PAPER]: 3,
-        [Player.SCISSORS]: 6,
-    },
-    [Opponent.SCISSORS]: {
-        [Player.ROCK]: 6,
-        [Player.PAPER]: 0,
-        [Player.SCISSORS]: 3,
-    },
-};
-
-const movesForOutcome = {
-    [Opponent.ROCK]: {
-        [DesiredOutcome.LOSE]: Player.SCISSORS,
-        [DesiredOutcome.DRAW]: Player.ROCK,
-        [DesiredOutcome.WIN]: Player.PAPER,
-    },
-    [Opponent.PAPER]: {
-        [DesiredOutcome.LOSE]: Player.ROCK,
-        [DesiredOutcome.DRAW]: Player.PAPER,
-        [DesiredOutcome.WIN]: Player.SCISSORS,
-    },
-    [Opponent.SCISSORS]: {
-        [DesiredOutcome.LOSE]: Player.PAPER,
-        [DesiredOutcome.DRAW]: Player.SCISSORS,
-        [DesiredOutcome.WIN]: Player.ROCK,
-    },
-};
-
-const getResultScore = ([opponent, player]: Round) => {
-    return moves[opponent][player];
-};
-
-const getOwnScore = (player: Player) => {
-    switch (player) {
-        case Player.ROCK:
-            return 1;
-        case Player.PAPER:
-            return 2;
-        case Player.SCISSORS:
-            return 3;
-        default:
-            assertNever(player);
-    }
-};
-
-export const staticRockPaperScissors = (lines: string[]) =>
-    getRounds<Round>(lines)
-        .map((round) => getResultScore(round) + getOwnScore(round[1]))
+const getRoundScoreSum = (rounds: Round[]) =>
+    rounds
+        .map((round) => getRoundScore(round) + getMoveScore(round[1]))
         .reduce((result, score) => result + score, 0);
 
-export const dynamicRockPaperScissors = (lines: string[]) =>
-    getRounds<DynamicRound>(lines)
-        .map(([opponent, desiredOutcome]) => [
-            opponent,
-            movesForOutcome[opponent][desiredOutcome],
-        ])
-        .map((round: Round) => getResultScore(round) + getOwnScore(round[1]))
-        .reduce((result, score) => result + score, 0);
+export const staticRockPaperScissors = (lines: string[]) => {
+    const rounds = getRounds<Round>(lines);
+    return getRoundScoreSum(rounds);
+};
+
+export const dynamicRockPaperScissors = (lines: string[]) => {
+    const rounds = getRounds<DynamicRound>(lines).map<Round>((round) => [
+        round[0],
+        getMoveForOutcome(round),
+    ]);
+    return getRoundScoreSum(rounds);
+};
