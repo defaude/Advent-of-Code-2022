82a4d92d91dcfaae11107f1742c3bd78b71e9d0e
Roland Hummel <defaude@gmail.com>
2022-12-04 19:56:55 +0000

    Add "non-blank" line reader

---
+++ utility/getNonBlankFileLines.ts
@@ -0,0 +1,7 @@
+import { getFileLines } from './getFileLines';
+
+export const getNonBlankFileLines = async (file: string, metaUrl: string) => {
+    return (await getFileLines(file, metaUrl)).filter(
+        (line) => line.trim().length > 0
+    );
+};
