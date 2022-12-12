3186aa82616939b270e186a14fb559f408878fb1
defaude <defaude@gmail.com>
2022-12-12 07:59:49 +0100

    Fix test naming

--- challenges/11/monkeyInTheMiddle.test.ts
+++ challenges/11/monkeyInTheMiddle.test.ts
@@ -6,11 +6,11 @@ describe('11', async () => {
     const example = await getFileLines('11-example.txt', import.meta.url);
 
     describe('getMonkeyBusinessLevel', () => {
-        it('calculate the correct monkey business level with Relief', () => {
+        it('should calculate the correct monkey business level with Relief', () => {
             expect(getMonkeyBusinessLevel(example, 20)).toBe(10605);
         });
 
-        it('calculate the correct monkey business level without Relief', () => {
+        it('should calculate the correct monkey business level without Relief', () => {
             expect(getMonkeyBusinessLevel(example, 10000, false)).toBe(2713310158);
         });
     });
